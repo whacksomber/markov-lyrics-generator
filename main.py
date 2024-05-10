7243d48e75d48ef3d8778edfb38c4b8a0486c69f
Isabelle Skivington <meioliver00@gmail.com>
2024-05-09 17:19:16 -0700

    condensed lyrics-grabbing loop into a function call

--- main.py
+++ main.py
@@ -4,38 +4,23 @@ from time import sleep # needed to avoid ban from alyrics
 import random
 import time
 
+disclaimer = "Usage of azlyrics.com content by any third-party lyrics provider is prohibited by our licensing agreement. Sorry about that. -->"
+
 lyrics_original = open('lyrics.txt', 'w')
 
-url = "https://www.azlyrics.com/c/cure.html"
+url = "https://www.azlyrics.com/c/cure.html" # url of the artist page on azlyrics
 
 def get_html_str (url):
     html = urllib.request.urlopen(url)
     htmlstr = str(html.read())
     return htmlstr
 
-start_time = time.time() # start time of the program
+def get_lyrics (url):
+    html_str = get_html_str(url)
 
-artist_html_str = get_html_str(url)
-
-links = re.findall('href="([^"]+)"', artist_html_str)
-
-songLinks = []
-
-for x in links:
-    if "lyrics/cure" in x:
-        x = "https://www.azlyrics.com" + x
-        songLinks.append(x)
-
-disclaimer = "Usage of azlyrics.com content by any third-party lyrics provider is prohibited by our licensing agreement. Sorry about that. -->"
-
-i = 0
-
-for x in songLinks:
-    song_html_str = get_html_str(x)
-    
-    split = song_html_str.split(disclaimer,1)
-    split_html = split[1] #get everything after the disclaimer
-    split = split_html.split('</div>',1) #get everything before the end of the lyrics
+    split = html_str.split(disclaimer,1)
+    split_html = split[1] # get everything after the disclaimer
+    split = split_html.split('</div>',1) # get everything before the end of the lyrics
     lyrics = split[0]
     lyrics = lyrics.replace('<br>', '\n')
     lyrics = lyrics.replace('\\', '')
@@ -44,10 +29,30 @@ for x in songLinks:
     lyrics = lyrics.replace('<i>', '')
     lyrics = lyrics.replace('</i>', '')
     lyrics = lyrics.replace('[Chorus]', '')
+    
+    return lyrics
+
+start_time = time.time() # start time of the program
+
+artist_html_str = get_html_str(url)
+
+links = re.findall('href="([^"]+)"', artist_html_str)
+
+song_links = []
+
+for x in links:
+    if "lyrics/cure" in x:
+        x = "https://www.azlyrics.com" + x
+        song_links.append(x)
+
+i = 0 # counter for the number of songs
+
+for x in song_links:    
+    lyrics = get_lyrics(x)
     lyrics_original.write(lyrics)
     i += 1
-    print(round(i / len(songLinks) * 100, 2), "%", end='\r')
-    sleep (random.randint(2, 15)) #sleep for a random amount of time between 2 and 10 seconds to avoid ban from azlyrics
-    
-lyrics_original.close()
-print("--- %s seconds ---" % round(time.time() - start_time, 2))
+    print(round(i / len(song_links) * 100, 2), "%", end='\r') # print the percentage of songs whose lyrics have been downloaded
+    sleep (random.randint(2, 15)) # sleep for a random amount of time between 2 and 10 seconds to avoid ban from azlyrics
+
+lyrics_original.close() # close the file
+print("--- %s seconds ---" % round(time.time() - start_time, 2)) # print the time it took to run the program
