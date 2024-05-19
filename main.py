2137f1809f2d61ffc47761517948fd78856bdd00
Isabelle Skivington <meioliver00@gmail.com>
2024-05-19 16:18:31 -0700

    Modified code to use BeautifulSoup for parsing HTML instead + other simplifications and stylistic modifications to code

--- main.py
+++ main.py
@@ -6,8 +6,9 @@ import random
 import time
 from alive_progress import alive_it
 import markovify
+from bs4 import BeautifulSoup
 
-DISCLAIMER = "Usage of azlyrics.com content by any third-party lyrics provider is prohibited by our licensing agreement. Sorry about that. -->"
+# DISCLAIMER = "Usage of azlyrics.com content by any third-party lyrics provider is prohibited by our licensing agreement. Sorry about that. -->"
 
 def get_html_str (url):
     try:
@@ -21,34 +22,25 @@ def get_html_str (url):
 def get_lyrics (url):
     html_str = get_html_str(url)
 
-    if html_str == None:
-        print('No lyrics page found for: ', url)
+    if not html_str:
+        print(f'No lyrics page found for: {url}')
         return None
-
-    split = html_str.split(DISCLAIMER,1)
-    split_html = split[1] # get everything after the disclaimer
-    split = split_html.split('</div>',1) # get everything before the end of the lyrics
-    lyrics = split[0]
-    lyrics = lyrics.replace('<br>', '\n')
-    lyrics = lyrics.replace('\\', '')
-    lyrics = lyrics.replace('\nn', '\n')
-    lyrics = lyrics.replace('\r', '')
-    lyrics = lyrics.replace('<i>', '')
-    lyrics = lyrics.replace('</i>', '')
-    lyrics = lyrics.replace('[Chorus]', '')
     
-    return lyrics
+    soup = BeautifulSoup(html_str, 'html.parser')
+    lyrics = soup.find('div', class_="col-xs-12 col-lg-8 text-center").find(lambda tag: tag.name == 'div' and not tag.attrs)
+    return re.sub(r'\[.*?\]', '', lyrics.text)
 
 def get_song_links(url):
     artist_html_str = get_html_str(url)
 
-    if artist_html_str == None:
+    if not artist_html_str:
         print('No artist page found')
         return []
 
-    # get all the links to the songs of the artist
-    song_links = re.findall(r'href="([^"]*lyrics/[^"]*)"', artist_html_str)
-    song_links = list(map(lambda x: "https://www.azlyrics.com" + x, song_links))
+    soup = BeautifulSoup(artist_html_str, 'html.parser')
+    
+    song_links = [f"https://www.azlyrics.com{link['href']}" for link in soup.find_all('a', href=True)if '/lyrics/' in link['href']]
+
     return song_links
 
 start_time = time.time() # start time of the program
@@ -65,7 +57,7 @@ print('Number of songs found: ', len(song_links))
 
 for x in alive_it(song_links):
     lyrics = get_lyrics(x)
-    if lyrics == None:
+    if not lyrics:
         continue
     lyrics_original.write(lyrics)
     sleep (random.randint(2, 10)) # sleep for a random amount of time between 2 and 10 seconds to avoid ban from azlyrics
