1fd107fd0f127d4ac22ef420fcf7904498f91681
Isabelle Skivington <meioliver00@gmail.com>
2024-05-19 17:47:31 -0700

    - Refactored some code blocks into functions to increase code modularity (for instance, generating Markov lines)\n- Used with statements for files for better resource management

--- main.py
+++ main.py
@@ -59,42 +59,42 @@ def get_user_input ():
         else:
             return url
 
+def generate_markov_lines(num_lines, file_name = "lyrics.txt"):
+    with open(file_name, 'r') as file:
+        text = file.read()
+
+        if text == '':
+            print('No lyrics found.')
+            exit()
+
+    markovifyTextModel = markovify.Text(text)
+
+    for i in range(num_lines):
+        line = markovifyTextModel.make_sentence()
+        print(line)
+
+def write_lyrics_file(url, file_name = "lyrics.txt"):
+    with open('lyrics.txt', 'w', encoding='utf-8') as lyrics_original:
+        song_links = get_song_links(url)
+
+        if song_links == []:
+            print('No songs found')
+            exit()
+
+        print('Number of songs found: ', len(song_links))
+
+        for x in alive_it(song_links):
+            lyrics = get_lyrics(x)
+            if not lyrics:
+                continue
+            lyrics_original.write(lyrics)
+            sleep (random.randint(2, 10)) # sleep for a random amount of time between 2 and 10 seconds to avoid ban from azlyrics
+
 start_time = time.time() # start time of the program
 
 url = get_user_input()
-
 num_lines = int(input('Enter the number of lines of lyrics you want to generate: '))
-
-lyrics_original = open('lyrics.txt', 'w', encoding='utf-8') # open a file to write the lyrics to
-song_links = get_song_links(url)
-
-if song_links == []:
-    print('No songs found')
-    exit()
-
-print('Number of songs found: ', len(song_links))
-
-for x in alive_it(song_links):
-    lyrics = get_lyrics(x)
-    if not lyrics:
-        continue
-    lyrics_original.write(lyrics)
-    sleep (random.randint(2, 10)) # sleep for a random amount of time between 2 and 10 seconds to avoid ban from azlyrics
-lyrics_original.close()
-
-file = open('lyrics.txt', 'r') # open the file with the lyrics in read mode
-text = file.read()
-
-if text == '':
-    print('No lyrics found.')
-    exit()
-
-file.close()
-
-markovifyTextModel = markovify.Text(text)
-
-for i in range(num_lines):
-    line = markovifyTextModel.make_sentence()
-    print(line)
+write_lyrics_file(url)
+generate_markov_lines(num_lines)
 
 print("--- %s seconds ---" % round(time.time() - start_time, 2)) # print the time it took to run the program
